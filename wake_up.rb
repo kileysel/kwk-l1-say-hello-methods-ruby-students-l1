fff1ecedcde6e1d504a5de621deb0677779c302f
kileysel <kiley@selmanhouse.com>
2018-06-05 16:54:03 +0000

    Done.

---
+++ wake_up.rb
@@ -0,0 +1,11 @@
+def wake_up
+  7.times do 
+  puts "turn alarm off"
+  puts "get out of bed"
+end
+  puts "walk to kitchen"
+  puts "make breakfast"
+  puts "eat breakfast"
+  puts "go and get ready for day"
+end
+wake_up
