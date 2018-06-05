d759e51ac0592a63d96e85a41f546b295a66fa27
kileysel <kiley@selmanhouse.com>
2018-06-05 17:24:45 +0000

    automatically backed up by learn

---
+++ greeting.rb
@@ -0,0 +1,5 @@
+def greeting(local_greeting, your_name, my_name, time_of_day)
+  puts "#{local_greeting} #{your_name},I am #{my_name}, how's your #{time_of_day}"
+end 
+
+greeting("hey", "esme", "kiley", "morning")
